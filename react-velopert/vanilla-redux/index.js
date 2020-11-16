d743fdc20864a22c9e11ae034b9abd1fe8f04ab0
jeongmu.park <jeongmupark@gmail.com>
2020-11-16 15:50:25 +0900

    16.2.6 리듀서 함수 정의

---
+++ react-velopert/vanilla-redux/index.js
@@ -0,0 +1,47 @@
+// 16.2.3 DOM 레퍼런스 만들기
+const divToggle = document.querySelector('.toggle');
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelector('#increase');
+const btnDecrease = document.querySelector('#decrease');
+
+// 16.2.4 액션 타입과 액션 생성 함수 정의
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = (difference) => ({ type: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+// 16.2.5 초깃값 설정
+const initialState = {
+  toggle: false,
+  count: 0,
+};
+
+// 16.2.6 리듀서 함수 정의
+function reducer(state = initialState, action) {
+  // action.type에 따라 다른 작업을 처리함
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state, // 불변성 유지를 해 주어야 합니다.
+        toggle: !state.toggle,
+      };
+
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+
+    default:
+      return state;
+  }
+}
