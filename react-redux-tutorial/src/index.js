c97ef54128f6296e9d4fdccc029667ef7d3bf6da
jeongmu.park <jeongmupark@gmail.com>
2020-11-17 15:23:43 +0900

    17.4.1 스토어 만들기

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
