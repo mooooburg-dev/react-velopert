84a8e5eeb800ac8730208afb7727a6a5df8b8dbf
jeongmu.park <jeongmupark@gmail.com>
2020-11-20 18:21:43 +0900

    17.7.4 TodosContainer를 Hooks로 전환하기

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { changeInput, insert, toggle, remove } from '../modules/todos';
+import { useSelector, useDispatch } from 'react-redux';
+import todos, { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
+import { useCallback } from 'react';
 
-const TodosContainer = ({ input, todos, changeInput, insert, toggle, remove }) => {
-  return <Todos input={input} todos={todos} onChangeInput={changeInput} onInsert={insert} onToggle={toggle} onRemove={remove} />;
-};
+// const TodosContainer = ({ input, todos, changeInput, insert, toggle, remove }) => {
+//   return <Todos input={input} todos={todos} onChangeInput={changeInput} onInsert={insert} onToggle={toggle} onRemove={remove} />;
+// };
 
-export default connect(
-  // 비구조화 할당을 통해 todos를 분리하여
-  // state.todos.input 대신 todos.input을 사용
-  ({ todos }) => ({
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
     input: todos.input,
     todos: todos.todos,
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove,
-  }
-)(TodosContainer);
+  }));
+
+  const dispatch = useDispatch();
+  const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [dispatch]);
+
+  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  const onToggle = useCallback((text) => dispatch(toggle(id)), [dispatch]);
+  const onRemove = useCallback((text) => dispatch(remove(id)), [dispatch]);
+
+  return <Todos input={input} todos={todos} onChangeInput={onChangeInput} onInsert={onInsert} onRemove={onRemove} />;
+};
+
+export default TodosContainer;
