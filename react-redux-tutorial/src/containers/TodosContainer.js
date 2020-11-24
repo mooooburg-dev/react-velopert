fd409e6ed84802e3241841b9a7ab536f7509ef1a
jeongmu.park <jeongmupark@gmail.com>
2020-11-24 12:15:15 +0900

    17.7.6 connect 함수와의 주요 차이점

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import todos, { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
 import { useCallback } from 'react';
+import useActions from '../lib/useActions';
 
 // const TodosContainer = ({ input, todos, changeInput, insert, toggle, remove }) => {
 //   return <Todos input={input} todos={todos} onChangeInput={changeInput} onInsert={insert} onToggle={toggle} onRemove={remove} />;
@@ -14,14 +15,16 @@ const TodosContainer = () => {
     todos: todos.todos,
   }));
 
-  const dispatch = useDispatch();
-  const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [dispatch]);
+  // const dispatch = useDispatch();
+  // const onChangeInput = useCallback((input) => dispatch(changeInput(input)), [dispatch]);
 
-  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
-  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
-  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+  // const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  // const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
+  // const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+  const [onChangeInput, onInsert, onToggle, onRemove] = useActions([changeInput, insert, toggle, remove], []);
 
   return <Todos input={input} todos={todos} onChangeInput={onChangeInput} onInsert={onInsert} onToggle={onToggle} onRemove={onRemove} />;
 };
 
-export default TodosContainer;
+export default React.memo(TodosContainer);
