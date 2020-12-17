8171add8e39bb7ad651e15fd4c127209826387be
jeongmu.park <jeongmupark@gmail.com>
2020-12-17 15:17:49 +0900

    19. 코드 스플리팅

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -5,9 +5,13 @@ import App from './App';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
+// import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
@@ -15,3 +19,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.unregister();
