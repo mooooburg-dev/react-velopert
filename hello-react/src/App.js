63b36f5845fbbf72ad5762a3c64e3dbc3c08417c
jeongmu.park <jeongmupark@gmail.com>
2021-03-01 15:25:03 +0900

    5. ref: DOM에 이름달기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 // import EventPractice from "./EventPractice"
-// import ValidationSample from "./ValidationSample"
+import ValidationSample from './ValidationSample';
 // import ScrollBox from './ScrollBox'
 // import Iteration from './IterationSample'
-import LifeCycleSample from './LifeCycleSample';
-import ErrorBoundry from './ErrorBoundary';
+// import LifeCycleSample from './LifeCycleSample';
+// import ErrorBoundry from './ErrorBoundary';
 
 function getRandomColor() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -22,17 +22,19 @@ class App extends Component {
   };
   render() {
     return (
+      <ValidationSample />
+
       // <div>
       //   <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
       //   <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
       // </div>
 
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundry>
-          <LifeCycleSample color={this.state.color}></LifeCycleSample>
-        </ErrorBoundry>
-      </div>
+      // <div>
+      //   <button onClick={this.handleClick}>랜덤 색상</button>
+      //   <ErrorBoundry>
+      //     <LifeCycleSample color={this.state.color}></LifeCycleSample>
+      //   </ErrorBoundry>
+      // </div>
     );
   }
 }
--- hello-react/src/ValidationSample.js
+++ hello-react/src/ValidationSample.js
@@ -1,35 +1,47 @@
-import React, { Component } from 'react'
-import './ValidationSample.css'
+import React, { Component } from 'react';
+import './ValidationSample.css';
 
 class ValidationSample extends Component {
   state = {
     password: '',
     clicked: false,
     validated: false,
-  }
+  };
 
   handleChange = (e) => {
     this.setState({
       password: e.target.value,
-    })
-  }
+    });
+  };
 
   handleButtonClick = () => {
     this.setState({
       clicked: true,
       validated: this.state.password === '0000',
-    })
+    });
 
-    this.input.focus()
-  }
+    this.input.focus();
+  };
   render() {
     return (
       <div>
-        <input ref={(ref) => (this.input = ref)} type="password" value={this.state.password} onChange={this.handleChange} className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''}></input>
+        <input
+          ref={(ref) => (this.input = ref)}
+          type='password'
+          value={this.state.password}
+          onChange={this.handleChange}
+          className={
+            this.state.clicked
+              ? this.state.validated
+                ? 'success'
+                : 'failure'
+              : ''
+          }
+        ></input>
         <button onClick={this.handleButtonClick}>검증하기</button>
       </div>
-    )
+    );
   }
 }
 
-export default ValidationSample
+export default ValidationSample;
